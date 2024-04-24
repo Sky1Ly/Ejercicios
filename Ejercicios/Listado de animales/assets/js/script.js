546b705549a584d52981ce3445cb99cd4917ac1a
Eider Agudelo <eider841agudelo@hotmail.com>
2024-04-24 18:02:41 -0400

    Se crea la clase propietario

---
+++ Ejercicios/Listado de animales/assets/js/script.js
@@ -0,0 +1,11 @@
+class Propietario{
+    constructor(nombre, direcion, telefono){
+        this._nombre = nombre
+        this._direccion = direcion
+        this._telefono = telefono
+    }
+
+    datosPropietario(){
+        
+    }
+}
