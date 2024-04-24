de44cd74fbb902965970b114b3067ca915410d14
Eider Agudelo <eider841agudelo@hotmail.com>
2024-04-24 16:27:18 -0400

    Se traen los datos con fetch y se muestran por consola

--- Ejercicios/promesas/assets/js/script.js
+++ Ejercicios/promesas/assets/js/script.js
@@ -1,1 +1,37 @@
 let header = document.getElementById("header")
+
+
+async function consultaData() {
+    let url = "https://jsonplaceholder.typicode.com/photos"
+    let options = { method: "GET" }
+
+    try {
+        const response = await fetch(url, options)
+        const result = await response.json()
+        return result
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function recorrerData() {
+    let data = await consultaData()
+
+    for (let i = 0; i < 20 && i < data.length; i++) {
+        console.log(data[i])
+    }
+}
+
+async function dataEnviada() {
+    setTimeout(() => {
+        muestraMensajeDataEnviada()
+    }, 3000)
+}
+
+async function muestraMensajeDataEnviada(){
+    console.log("Los datos han sido enviados")
+}
+
+dataEnviada()
+recorrerData()
